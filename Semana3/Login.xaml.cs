60cc67996705d9e587ef61bb2678bc31cccd4c35
Javier <Javier@JAVIER>
2023-04-25 17:41:22 -0500

    Agregar archivos de proyecto.

---
+++ Semana3/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string contrasena = txtContrasena.Text;
+            if (usuario == "jvalladares" && contrasena == "12345")
+            {
+                Navigation.PushAsync (new Principal());
+
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+                txtUsuario.Text = "";
+                txtContrasena.Text = "";
+            }
+        }
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Registro());
+        }
+    }
+}
